1ccd7c481030fdd7ac3ef5b183672f4e97656759
Mike <paulrimiru@gmail.com>
2016-09-25 17:49:04 +0300

    saving work. Not working

---
+++ Budd/Budd/Models/UnitsActivities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budd.Models
+{
+    public class UnitsActivities
+    {
+        public string name { get; set; }
+        public string time { get; set; }
+    }
+}
